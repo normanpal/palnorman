da2cb3c82f779211730a39faa2dde290d6d6f159
Norman <123292503+normanpal@users.noreply.github.com>
2023-12-23 09:27:18 +0100

    Delete _utils directory

--- _utils/collections/index.js
+++
@@ -1,9 +0,0 @@
-const {memoize} = require('./memo');
-
-module.exports = function(eleventyConfig) {
-
-    eleventyConfig.addCollection("memoized", function(collectionApi) {
-        return memoize(collectionApi.getAll())
-    })
-
-}
